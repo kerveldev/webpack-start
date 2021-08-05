a813f29f71bcf86bbecb7e497ad43188fbf73475
Irek Pelaez <kerveldev@gmail.com>
2021-08-05 13:54:20 -0500

    Inicializacion del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output:{
+        clean: true,
+    },
+    module:{
+        rules:[
+            {
+                test: /\.html$/i,
+                loader:'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false,
+                }
+            },
+            {
+                test:/\.css$/i,
+                exclude:/styles.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/styles.css$/,
+                use:[
+                    MiniCssExtract.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(png|jp?g|gif)$/,
+                loader: 'file-loader'
+            }
+            
+        ]
+    },
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            template: './src/index.html',
+            //filename: './index.html'
+        }),
+
+        new MiniCssExtract({
+            filename:'[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {from:'src/assets/', to:'assets/'}
+            ]
+        })
+    ]
+
+}
